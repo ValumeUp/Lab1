170955fb67eb09b13af346f939cb67b6736059c6
Valeriia <valeriia.bykovets@ukma.edu.ua>
2021-04-05 20:45:51 +0300

    Login Interface

--- Lab1_Tests/UserTest.cs
+++ Lab1_Tests/UserTest.cs
@@ -13,7 +13,7 @@ namespace Lab1_Tests
             var  user1= new User { Name = "David", Surname="Black", Email="davBl@gmail.com" };
             var actual = user1.Validate();
             Assert.True(actual);
-            Assert.Equal(user1.Id, 1);
+            Assert.Equal(user1.Guid, 1);
             var wallet = new Wallet { Name = "masha", Currency = "usd", InitialBalance = 200, Description = "ygjgg", UserId = 1 };
             user1.Cards.Add(wallet);
             var str=user1.Show();
