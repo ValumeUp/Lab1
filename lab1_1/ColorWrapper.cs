2fb77c4a7bc978c64fb169273daf935c04a4f732
Mariia <masha110202@gmail.com>
2021-04-19 17:10:13 +0300

    lab11 changed

---
+++ lab1_1/ColorWrapper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1_1
+{
+    public class ColorWrapper
+    {
+        public Color Color { get; set; }
+
+
+        public ColorWrapper(Color color)
+        {
+            Color = color;
+        }
+    }
+}
